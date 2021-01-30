15be4176ea0fcc23fcf87c04045cbbb5c257faf9
inmerso <luca.rizzon@gmail.com>
2021-01-30 21:32:40 +0100

    mod

--- runningStatistics.py
+++ runningStatistics.py
@@ -11,18 +11,19 @@ def running_variance(x, i, mean, msq):
     msq   = msq + delta * ( x - mean )
     return msq, mean, delta
 
-def compute_statistics(samples):
+def calcAverage(s):
+    sum = 0
+    for i in range( len(s) ):
+        sum = sum + s[i]
+    return sum/len(s)
+
+def calcVariance(s):
     sum = 0
     sumsqrd = 0
-    for i in range(n):
-        sum = sum + samples[i]
-        sumsqrd = sumsqrd + samples[i] * samples[i]
-
-    average = sum/n
-    print("mean:    ", average)
-
-    variance = ( sumsqrd + n * average * average - 2 * average * sum ) / n
-    print("variance:        ", variance)
+    for i in range( len(s) ):
+        sum = sum + s[i]
+        sumsqrd = sumsqrd + s[i] ** 2
+    return (sumsqrd + n * (calcAverage(s)) ** 2 - 2 * (calcAverage(s)) * sum )/len(s)
 
 if __name__ == "__main__":
 
@@ -30,8 +31,8 @@ if __name__ == "__main__":
     n = len( samples )
     print("num samples: ", n)
 
-
-    compute_statistics( samples )
+    print("Average: ", calcAverage(samples))
+    print("Variance: ", calcVariance(samples))
 
     # Running algorithms
     i = 0
@@ -40,7 +41,7 @@ if __name__ == "__main__":
     for x in samples:
         i = i + 1
         mean, delta = running_mean(x, i, mean, delta)
-    print("mean: ", mean)
+    print("Running Average: ", mean)
 
     i = 0
     mean = 0
@@ -49,4 +50,4 @@ if __name__ == "__main__":
     for x in samples:
         i = i + 1
         msq, mean, delta = running_variance(x, i, mean, msq)
-    print("variance: ", msq / n)
+    print("Running Variance: ", msq / n)
